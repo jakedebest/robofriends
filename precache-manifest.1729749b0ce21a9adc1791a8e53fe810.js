a6da4e1c29037d49d32289d582b8e75755af6567
jakedebest <jwinnett@tutanota.com>
2020-09-27 15:05:50 +0100

    Updates

---
+++ precache-manifest.1729749b0ce21a9adc1791a8e53fe810.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb99918dbc68e41022769e81f6654fe",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "71aebea9923dd91be2fd",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71aebea9923dd91be2fd",
+    "url": "/robofriends/static/js/main.722e1834.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
